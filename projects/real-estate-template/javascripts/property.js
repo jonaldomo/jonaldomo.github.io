725ad9af5f4c8fdc18b98ef6a6773d18b1444e83
John Moses <john.moses@cerner.com>
2014-03-02 23:30:25 -0600

    new pictures for real estate template

--- projects/real-estate-template/javascripts/property.js
+++ projects/real-estate-template/javascripts/property.js
@@ -14,4 +14,23 @@ $(document).ready(function(){
   $('.bxslider').bxSlider({
     pagerCustom: '#bx-pager'
   });
+  
+  var map,
+      myLatLng = new google.maps.LatLng(37.7833, -122.4167),
+      myLatLng1 = new google.maps.LatLng(37.7733, -122.4067),
+      myLatLng2 = new google.maps.LatLng(37.7633, -122.4667),
+      myLatLng3 = new google.maps.LatLng(37.7933, -122.3967),
+      myLatLng4 = new google.maps.LatLng(37.7433, -122.4167);
+
+  function initialize() {
+    var mapOptions = {
+      zoom: 11,
+      scrollwheel: false,
+      center: myLatLng
+    };
+    map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
+    }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 });
